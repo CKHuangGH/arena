187de0142c16f11ef8bb4660d04350422064c07c
CKHuang-public <chihkaihuang1@gmail.com>
2025-08-08 20:49:57 +0200

    ww

--- cluster_deployment/02_system_ready.sh
+++ cluster_deployment/02_system_ready.sh
@@ -2,14 +2,14 @@
 
 manage=$(awk NR==1 node_list)
 
-git clone https://github.com/CKHuangGH/mqtt
+git clone https://github.com/CKHuangGH/arena
 
 rm -rf /home/chuang/.ssh/known_hosts
 
 for j in $(cat node_list)
 do
     scp /home/chuang/.ssh/id_rsa root@$j:/root/.ssh
-    scp -r ./mqtt root@$j:/root/
+    scp -r ./arena root@$j:/root/
     scp -r /home/chuang/bench_ctrl root@$j:/root/
 done
 
@@ -20,11 +20,11 @@ i=0
 for j in $(cat node_list)
 do
 ssh -o StrictHostKeyChecking=no root@$j scp -o StrictHostKeyChecking=no /root/.kube/config root@$manage:/root/.kube/cluster$i
-ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/mqtt/
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/arena/
 
 i=$((i+1))
 done
 
-scp node_list root@$manage:/root/mqtt/package/node_list
+scp node_list root@$manage:/root/arena/package/node_list
 
 echo "management node is $manage"
