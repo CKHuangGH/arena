ca83ce94bcd86dab2e7408dfcc288be746e674dc
CKHuangGH <chihkaihuang1@gmail.com>
2025-08-18 15:51:08 +0200

    Update 01_deploy.sh

--- package/01_deploy.sh
+++ package/01_deploy.sh
@@ -5,7 +5,7 @@ helm install cilium cilium/cilium \
   --namespace kube-system \
   --wait --wait-for-jobs \
   --set operator.replicas=1 \
-  --set operator.nodeSelector."kubernetes\.io/hostname"=control-plane \
+  --set operator.nodeSelector."node-role\.kubernetes\.io/control-plane"="" \
   --set operator.tolerations[0].key=node-role.kubernetes.io/control-plane \
   --set operator.tolerations[0].operator=Exists \
   --set operator.tolerations[0].effect=NoSchedule
@@ -22,12 +22,13 @@ echo -e "\rTime's up!     "
 
 kubectl -n kube-system patch deploy coredns --type=merge -p '{
   "spec": { "template": { "spec": {
-    "nodeSelector": { "kubernetes.io/hostname": "control-plane" },
+    "nodeSelector": { "node-role.kubernetes.io/control-plane": "" },
     "tolerations": [
       { "key":"node-role.kubernetes.io/control-plane","operator":"Exists","effect":"NoSchedule" }
     ]
   } } }
 }'
+
 
 echo "wait 30 secs"
 for i in $(seq 30 -1 1); do
@@ -51,17 +52,17 @@ helm install prometheus prometheus-community/kube-prometheus-stack \
   --set prometheus.prometheusSpec.scrapeInterval="5s" \
   --set prometheus.prometheusSpec.enableAdminAPI=true \
   \
-  --set prometheus.prometheusSpec.nodeSelector."kubernetes\.io/hostname"=control-plane \
+  --set operator.nodeSelector."node-role\.kubernetes\.io/control-plane"="" \
   --set prometheus.prometheusSpec.tolerations[0].key=node-role.kubernetes.io/control-plane \
   --set prometheus.prometheusSpec.tolerations[0].operator=Exists \
   --set prometheus.prometheusSpec.tolerations[0].effect=NoSchedule \
   \
-  --set prometheusOperator.nodeSelector."kubernetes\.io/hostname"=control-plane \
+  --set operator.nodeSelector."node-role\.kubernetes\.io/control-plane"="" \
   --set prometheusOperator.tolerations[0].key=node-role.kubernetes.io/control-plane \
   --set prometheusOperator.tolerations[0].operator=Exists \
   --set prometheusOperator.tolerations[0].effect=NoSchedule \
   \
-  --set kube-state-metrics.nodeSelector."kubernetes\.io/hostname"=control-plane \
+  --set operator.nodeSelector."node-role\.kubernetes\.io/control-plane"="" \
   --set kube-state-metrics.tolerations[0].key=node-role.kubernetes.io/control-plane \
   --set kube-state-metrics.tolerations[0].operator=Exists \
   --set kube-state-metrics.tolerations[0].effect=NoSchedule
